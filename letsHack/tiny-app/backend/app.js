7c4164f9b5630c94ef4c48c2d40c753cff8a6b64
AlexeyS <xengawe@gmail.com>
2021-04-08 12:06:58 -0700

    make http request

--- letsHack/tiny-app/backend/app.js
+++ letsHack/tiny-app/backend/app.js
@@ -3,13 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 
 const db = require('./config/keys').mongoURI
+const question = require('./routes/api/questions')
+app.use(express.json())
 
 mongoose
   .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('Mongo is gogo'))
   .catch(err => console.log(err))
 
+  
 app.get('/', (req, res) => res.send('This works'))
+app.use('/api/questions', question)
+
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server runs on port: ${port}`))
