cc12811f0a93e5a18f3639044dbbdcd76059a59c
AlexeyS <xengawe@gmail.com>
2021-04-14 22:12:36 -0700

    add more leetcode ez problems

--- DSA_AAopen/leetCode.js
+++ DSA_AAopen/leetCode.js
@@ -60,3 +60,98 @@ var kidsWithCandies = function(candies, extraCandies) {
 };
 
 
+
+
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+ 
+var shuffle = function(nums, n) {
+    let newArr = []
+    
+    let left = nums.slice(0, n);
+    let right = nums.slice(n);
+    i = 0;
+    while  (i < n) {
+        newArr.push(left[i])
+        newArr.push(right[i])
+        i++
+    }
+    return newArr
+};
+
+// const shuffle = function(nums, n) {
+//     return nums.slice(0,n).reduce((acc, curr, i) => [...acc, curr, nums[i+n]],[])
+// };
+
+
+// Given an array of integers nums.
+
+// A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+// Return the number of good pairs.
+
+
+var numIdenticalPairs = function(nums) {
+    let hash = {} // 1:3, 2:1, 3:2, 
+    
+    let count = 0; // 4
+    for (let i = 0; i < nums.length; i++){
+        if (hash[nums[i]]) {
+            count += hash[nums[i]]
+            hash[nums[i]]++
+        } else {
+            hash[nums[i]] = 1;
+        }
+        
+    }
+    return count
+};
+
+
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Clarification:
+
+// Confused why the returned value is an integer but your answer is an array?
+
+// Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+
+// Internally you can think of this:
+
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;    // [1, 2, 2]
+                                        //    i^ j^
+    
+    let i = 0;
+    for(let j = 1; j < nums.length; j++){
+        if (nums[j] !== nums[i]){
+            i++;
+            nums[i] = nums[j]
+        }
+    }
+    console.log(nums)
+    return i + 1
+};
+
+// Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+// Initially target array is empty.
+// From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+// Repeat the previous step until there are no elements to read in nums and index.
+// Return the target array.
+
+// It is guaranteed that the insertion operations will be valid.
+
+
+var createTargetArray = function(nums, index) {
+    let newArr = [];
+    
+    for (let i = 0; i < nums.length; i++){
+        newArr.splice(index[i], 0, nums[i])    
+    }
+    
+    return newArr;
+};
