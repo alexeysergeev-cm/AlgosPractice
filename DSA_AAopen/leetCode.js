65c00517a3e681c126afc6477a342f38b9935e43
AlexeyS <xengawe@gmail.com>
2021-04-16 14:32:43 -0700

    add array String are equal

--- DSA_AAopen/leetCode.js
+++ DSA_AAopen/leetCode.js
@@ -326,3 +326,11 @@ var smallerNumbersThanCurrent = function(nums) {
     //let res = nums.map(num => sorted.indexOf(num))
     return res;
 };
+
+
+// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+// A string is represented by an array if the array elements concatenated in order forms the string.
+
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.join('') === word2.join('')
+};
