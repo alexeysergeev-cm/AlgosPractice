b9b36c1f849b6eab7269e48267d076cdc68e9e86
AlexeyS <xengawe@gmail.com>
2021-04-16 16:04:55 -0700

    add without arr linkedList with cycle

--- DSA_AAopen/leetCode.js
+++ DSA_AAopen/leetCode.js
@@ -359,3 +359,19 @@ var hasCycle = function(head) {
     return false
     
 };
+
+//also bruteforce but without arr
+var hasCycle = function(head) {
+    let slow = head
+    let fast = head
+    
+    while(fast !== null && fast.next !== null){
+        slow = slow.next
+        fast = fast.next.next;
+        
+        if (slow === fast) return true
+    }
+    
+    return false 
+    
+};
