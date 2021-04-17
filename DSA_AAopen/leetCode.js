d6a7bb0d583bf469fadd43de5560c453fd958476
AlexeyS <xengawe@gmail.com>
2021-04-17 16:37:28 -0700

    merge 2 sorted lists

--- DSA_AAopen/leetCode.js
+++ DSA_AAopen/leetCode.js
@@ -478,3 +478,25 @@ function convert(num){
 
   return res.join('')
 }
+
+
+//merge two sorted lists
+//l1 = [1,2,3]
+//l2 = [1,3,4]
+var mergeTwoLists = function(l1, l2) {
+
+    const head = new ListNode(null);
+    let curr = head;
+    while(l1 && l2){
+        if(l1.val < l2.val){
+            curr.next = l1
+            l1 = l1.next
+        } else {
+            curr.next = l2
+            l2 = l2.next
+        }
+        curr = curr.next;
+    }
+    curr.next = l1 || l2
+    return head.next  
+};
