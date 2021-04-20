f76253e00b289e3b7816828a9122e1efdfef521b
AlexeyS <xengawe@gmail.com>
2021-04-20 11:47:32 -0700

    add is Max heap

--- DSA_AAopen/aaopen.js
+++ DSA_AAopen/aaopen.js
@@ -1152,3 +1152,22 @@ class MaxHeap {
         return max
     }
 }
+
+
+///is Max heap?
+
+function isMaxHeap(array) {
+    // check if the tree is complete, i.e. there are no gaps like [null, 50, undefined, 20]
+    let isComplete = array.every(el => el !== undefined);
+    return isComplete && _isMaxHeap(array);
+}
+
+function _isMaxHeap(array, idx=1) {
+    if (array[idx] === undefined) return true;
+    let leftIdx = 2 * idx;
+    let rightIdx = 2 * idx + 1;
+    let leftVal = array[leftIdx] === undefined ? -Infinity : array[leftIdx];
+    let rightVal = array[rightIdx] === undefined ? -Infinity : array[rightIdx];
+    return array[idx] > leftVal && array[idx] > rightVal 
+        && _isMaxHeap(array, leftIdx) && _isMaxHeap(array, rightIdx);
+}
