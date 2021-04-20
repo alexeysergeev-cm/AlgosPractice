010a24f9d9ba784c10adbe30e12cd8c13f4b9d1d
AlexeyS <xengawe@gmail.com>
2021-04-19 18:01:32 -0700

    add trie

--- DSA_AAopen/aaopen.js
+++ DSA_AAopen/aaopen.js
@@ -928,3 +928,49 @@ function heapify(array, n, i) {
 function swap(array, i, j) {
     [ array[i], array[j] ] = [ array[j], array[i] ];
 }
+
+
+//Trie
+
+class Node {
+    constructor() {
+        this.children = {};
+        this.isTerminal = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new Node();
+    }
+
+    insert(word, root=this.root) {
+        let letter = word[0];
+        if (!(letter in root.children)) {
+            root.children[letter] = new Node();
+        }
+
+        if (word.length === 1) {
+            root.children[letter].isTerminal = true;
+        } else {
+            this.insert(word.slice(1), root.children[letter]);
+        }
+    }
+
+    search(word, root=this.root) {
+        if (word.length === 0) {
+            if (root.isTerminal) {
+                return true;
+            } else {
+                return false;
+            }
+        } 
+        
+        let letter = word[0];
+        if (letter in root.children) {
+            return this.search(word.slice(1), root.children[letter]);
+        } else {
+            return false;
+        }
+    }
+}
