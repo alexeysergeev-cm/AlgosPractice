11ee3ffb95868b748e420a2f2fd1abf4804b94ca
AlexeyS <xengawe@gmail.com>
2021-04-23 09:38:39 -0700

    remove dups from linkedlist

--- DSA_AAopen/leetCode.js
+++ DSA_AAopen/leetCode.js
@@ -693,3 +693,22 @@ var addTwoNumbers = function (l1, l2) {
     return newNode
 
 };
+
+
+
+/// remove dups from linkedlist
+var deleteDuplicates = function(head) {
+    
+    let cur = head
+
+    while (cur && cur.next) {
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next
+        } else {
+            cur = cur.next
+        }
+        
+    }
+    
+    return head
+};
