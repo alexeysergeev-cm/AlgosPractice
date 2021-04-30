d0fc3b9c1d7a81d142fdad204b390df3fd242920
AlexeyS <xengawe@gmail.com>
2021-04-30 15:01:52 -0700

    reverse string 3

---
+++ DSA_AAopen/leetCode_part2.js
@@ -0,0 +1,8 @@
+// Given a string s, reverse the order of characters in
+//  each word within a sentence while still preserving whitespace and initial word order.
+
+var reverseWords = function(s) {
+    return s.split(' ').map(word => {
+        return word.split('').reverse().join('')
+    }).join(' ')
+};
