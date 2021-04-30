9919c9c1c8bc4793b2a2cf09b8d1e91b31b9b556
AlexeyS <xengawe@gmail.com>
2021-04-30 15:54:02 -0700

    reverse string 2

--- DSA_AAopen/leetCode_part2.js
+++ DSA_AAopen/leetCode_part2.js
@@ -6,3 +6,18 @@ var reverseWords = function(s) {
         return word.split('').reverse().join('')
     }).join(' ')
 };
+
+
+//reverse string 2
+
+const flipEveryEvenChunk = (c, i) => i % 2 == 0 ? c.split("").reverse().join("") : c
+
+var reverseStr = function(s, k) {
+    
+    let arr = [];
+    for(let i = 0; i < s.length / k; i++) {
+        arr.push(s.slice(i * k, i * k + k))
+    }
+    
+    return arr.map(flipEveryEvenChunk).join("")
+};
