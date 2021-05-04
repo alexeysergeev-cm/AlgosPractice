791f6cd6848f0539628477b7cf01d0cb87ef7df7
AlexeyS <xengawe@gmail.com>
2021-05-03 18:34:30 -0700

    find path

--- DSA_AAopen/leetCode_part2.js
+++ DSA_AAopen/leetCode_part2.js
@@ -89,3 +89,26 @@ function isPalin(s){
     
     return true
 }
+
+
+
+//find destination path
+
+var destCity = function(paths) {
+    
+    let f;
+    let set = new Set();
+    
+    for (let i = 0;i < paths.length; i++){
+        set.add(paths[i][0]);
+    }
+    
+    for (let i = 0; i < paths.length; i++){
+         if (!set.has(paths[i][1])){
+               f = paths[i][1];
+               break;
+         }
+    }
+    return f;  
+    
+};
