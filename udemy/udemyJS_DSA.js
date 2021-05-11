a8443361da445b9c29599a93fea0ba11b580fd3c
AlexeyS <xengawe@gmail.com>
2021-05-11 11:07:47 -0700

    one liner

--- udemy/udemyJS_DSA.js
+++ udemy/udemyJS_DSA.js
@@ -7,3 +7,63 @@ addUpto(1000000000000)
 let t2 = new Date()
 
 console.log(`Time elapsed: ${(t2 - t1) / 1000 } seconds.`)
+
+
+
+
+///same frequency
+
+function sameFrequency(n1,n2){
+  // good luck. Add any arguments you deem necessary.
+  const num1 = n1.toString();
+  const num2 = n2.toString();
+  if (num1.length !== num2.length) return false;
+  const obj = {};
+  
+  for(const char of num1){
+      obj[char] ? obj[char]++ : obj[char] = 1;
+  }
+  
+  for(const char of num2){
+      if(!obj[char]) {
+          return false;
+      } else {
+          obj[char]--
+      }
+  }
+  
+  return true
+}
+
+
+///are there duplicates
+
+function areThereDuplicates() {
+  // good luck. (supply any arguments you deem necessary.)
+  let arr = Array.from(arguments);
+  const obj = {};
+  
+  arr.forEach(ele => obj[ele] ? obj[ele]++ : obj[ele] = 1);
+  return Object.values(obj).some(ele => ele > 1)
+}
+
+
+// areThereDuplicates Solution (Multiple Pointers)
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a,b) => a > b);
+  let start = 0;
+  let next = 1;
+  while(next < args.length){
+    if(args[start] === args[next]){
+        return true
+    }
+    start++
+    next++
+  }
+  return false
+}
+// areThereDuplicates One Liner Solution
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
+}
