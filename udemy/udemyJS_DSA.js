0dbd544cbc9292797ff8294cd2e0193c17427eb8
AlexeyS <xengawe@gmail.com>
2021-05-13 17:32:43 -0700

    buble sort

--- udemy/udemyJS_DSA.js
+++ udemy/udemyJS_DSA.js
@@ -233,3 +233,18 @@ function naivePatternSearch(s, p) {
 // console.log(naivePatternSearch(str, 'er'))
 // console.log(naivePatternSearch(str, 'bob'))
 // console.log(naivePatternSearch(str, 'isher'))
+
+///buble sort
+
+function bubleSort(arr){
+  for(let i = arr.length; i > 0; i--){
+    for(let j = 0; j < i - 1; j++){
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubleSort([1,2,41,51,25,6,73,8,89,90,-2,3,-6]))
