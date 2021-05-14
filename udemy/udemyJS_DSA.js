ef0abb85f2c5f7b1eddff7867fb0884caf48a222
AlexeyS <xengawe@gmail.com>
2021-05-13 17:39:22 -0700

    optimize bubble sort

--- udemy/udemyJS_DSA.js
+++ udemy/udemyJS_DSA.js
@@ -237,12 +237,16 @@ function naivePatternSearch(s, p) {
 ///buble sort
 
 function bubleSort(arr){
+  let noSwaps;
   for(let i = arr.length; i > 0; i--){
+    noSwaps = true;
     for(let j = 0; j < i - 1; j++){
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
