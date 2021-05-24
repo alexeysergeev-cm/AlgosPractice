334b5b941ebe3467a43ca6140a2f055a3f6cc976
AlexeyS <xengawe@gmail.com>
2021-05-24 16:32:29 -0700

    can jump

--- LeetCode/LeetCode.js
+++ LeetCode/LeetCode.js
@@ -711,3 +711,16 @@ var trimMean = function(arr) {
 
     return (newA.reduce((a,b)=>a+b) / newA.length);
 };
+
+//can jump
+
+var canJump = function(nums) {
+    let max = 0;
+    
+    for(let i = 0; i < nums.length; i++){
+        if (i > max) return false;
+        if (i + nums[i] >= nums.length-1) return true;
+        max = Math.max(max, i+nums[i])
+    }
+    
+};
