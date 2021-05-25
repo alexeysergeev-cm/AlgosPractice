d3c41cdcb7e38a7ecdd50d0cb45d99a067850c77
AlexeyS <xengawe@gmail.com>
2021-05-25 13:54:01 -0700

    remove dups from linkedlist

--- LeetCode/LeetCode.js
+++ LeetCode/LeetCode.js
@@ -746,3 +746,21 @@ var removeElement = function(nums, val) {
     }
     return nums.length
 };
+
+///remove dups from linked list
+
+var deleteDuplicates = function(head) {
+    
+    let cur = head
+
+    while (cur && cur.next) {
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next
+        } else {
+            cur = cur.next
+        }
+        
+    }
+    
+    return head
+};
