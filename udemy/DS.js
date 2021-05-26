d8b2e2fc1f2585d48c6c0fcd89056c27df21e02d
AlexeyS <xengawe@gmail.com>
2021-05-25 19:48:27 -0700

    implement Queue

--- udemy/DS.js
+++ udemy/DS.js
@@ -362,6 +362,8 @@ class DoublyLinkedList{
 // console.log(dlist)
 
 
+//// -----------> Stack
+
 class Node{
   constructor(val){
     this.val = val
@@ -400,5 +402,41 @@ class Stack {
     this.size--
     return temp.val;
   }
-  
+
+}
+
+
+///-------------> Queue
+
+class Queue{
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val){
+    const newNode = new Node(val);
+    if(!this.size){
+      this.first = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+
+    return ++this.size;
+  }
+
+  dequeue(){
+    if(!this.first) return null;
+    let temp = this.first;
+    if(this.first === this.last){
+      this.last = null;
+    }
+    this.first = this.first.next
+
+    this.size--;
+    return temp.val;
+  }
 }
