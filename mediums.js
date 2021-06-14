222adb49a6b9e82e7b7d98f4a44591fc01221661
AlexeyS <xengawe@gmail.com>
2021-06-14 11:40:03 -0700

    do knight travails

--- mediums.js
+++ mediums.js
@@ -1,5 +1,43 @@
 function minMoves(n, startRow, startCol, endRow, endCol){
   //do bfs coz edges are not weighted;
+  if (n === 0) return -1;
 
-  const dirs = [[-2,1],[-1,2],[1,2], [2,1], [2,-1],[1,-2],[-1,-2],[-2,-1]]
+  const dirs = [
+    [-2,1],
+    [-1,2],
+    [1,2],
+    [2,1],
+    [2,-1],
+    [1,-2],
+    [-1,-2],
+    [-2,-1]
+  ]
+
+  let queue = [[startRow, startCol, 0]];
+  let seen = {};
+
+  while(queue.length){
+
+    let cur = queue.shift();
+    if(cur[0] === endRow && cur[1] === endCol){
+      return cur[2];
+    }
+
+    for(let move of dirs){
+      let nextMove = [cur[0]+move[0], cur[1]+move[1]]
+      let x = nextMove[0]
+      let y = nextMove[1]
+      if(x >= 0 && x < n && y >= 0 && y < n && !seen[nextMove]) {
+        seen[nextMove] = true;
+        queue.push(nextMove.concat(cur[2]+1));
+      }
+    }
+  }
+
+  return -1;
 }
+
+console.log(minMoves(10,0,0,0,2))
+console.log(minMoves(6,5,1,0,5))
+console.log(minMoves(9,4,4,4,8))
+console.log(minMoves(9,4,4,4,4))
