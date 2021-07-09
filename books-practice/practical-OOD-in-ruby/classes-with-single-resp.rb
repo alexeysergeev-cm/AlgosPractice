a52b1f723c4b594e84ea24564612fa51e2e27add
AlexeyS <xengawe@gmail.com>
2021-07-08 22:11:30 -0700

    addd revealing reference and usage of Struct

--- books-practice/practical-OOD-in-ruby/classes-with-single-resp.rb
+++ books-practice/practical-OOD-in-ruby/classes-with-single-resp.rb
@@ -12,3 +12,27 @@ class Gear
 end
 
 puts Gear.new(52, 11).ratio
+
+class RevealingReference
+  attr_reader :wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect { |wheel|
+      wheel.rim + (wheel.tire * 2)
+    }
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect {|cell|
+      Wheel.new(cell[0], cell[1])
+    }
+  end
+  
+end
+
+puts RevealingReference.new([[55,11], [55,30]]).wheels
+puts RevealingReference.new([[55,11], [55,30]]).diameters
