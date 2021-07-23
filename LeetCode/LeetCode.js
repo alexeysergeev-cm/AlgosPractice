8f1c7fb89fb8328e9e57b648436db0e40fe3a32d
AlexeyS <xengawe@gmail.com>
2021-07-23 10:09:09 -0700

    leter combo of a phone number

--- LeetCode/LeetCode.js
+++ LeetCode/LeetCode.js
@@ -1116,3 +1116,43 @@ var threeSum = function(nums) {      //    i  x     y
     }
     return res
 };
+
+
+///letter combo of a phone number
+
+var letterCombinations = function(digits) {
+    if(!digits.length) return [];
+    const res = [];
+    const letters = { 
+        0: "",
+        1: "",
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz",
+    }
+    
+    dfs(digits, letters, res, "", 0); 
+    
+    return res;
+};
+
+const dfs = (digits, letters, res, str, idx) => { //str =ad, idx = 2
+    if(idx === digits.length) { //true
+        res.push(str); //['ad', 'ae', 'af']
+        return;
+    }
+    
+    let digit = digits[idx]; //3
+    let chars = letters[digit];//'def'
+    
+    for(const char of chars){ //abc
+        str += char; //b
+        dfs(digits, letters, res, str, idx+1); //idx = 0
+        str = str.slice(0, str.length-1); //delete ae => a
+    }
+}
