156b77e647cdc6e1212d4c482cea792bb5bcf5da
AlexeyS <xengawe@gmail.com>
2021-07-28 18:33:01 -0700

    add hello world in c

---
+++ cs50/hello.c
@@ -0,0 +1,10 @@
+// #include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // string ans = get_string("What's your name? ");
+    // printf("hello, %s\n", ans);
+    printf("hello, world\n");
+}
+
