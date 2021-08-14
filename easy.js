1075bc3ec4c90049cbe2df844a47225f17034139
AlexeyS <xengawe@gmail.com>
2021-08-14 11:30:48 -0700

    find intersection between 2 arrays

--- easy.js
+++ easy.js
@@ -20,3 +20,29 @@ var stringMatching = function(words) {
 
 const stringMatching = words =>
   words.filter(n => words.some(h => h !== n && h.includes(n)));
+
+
+
+// find intersection between 2 arrays
+
+var intersection = function(nums1, nums2) {
+    nums1.sort((a,b) => a-b);
+    nums2.sort((a,b) => a-b);
+    
+    let result = [];
+    let x = 0, y = 0;
+    
+    while (x < nums1.length && y < nums2.length ) {
+        if(nums1[x] === nums2[y] && !result.includes(nums1[x])) {
+            result.push(nums1[x]);
+            x++;
+            y++;
+        } else if (nums1[x] < nums2[y]){
+            x++;
+        } else {
+            y++;
+        }
+    }
+    
+    return result;
+};
