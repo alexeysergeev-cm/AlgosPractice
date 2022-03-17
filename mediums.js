9aa982b5f92024aafcce3b6c3ba73c5ddde7f469
Alexey Sergeev <xengawe@gmail.com>
2022-03-17 10:31:12 -0700

    binarySearch

--- mediums.js
+++ mediums.js
@@ -1123,3 +1123,120 @@ function bfsMatrixTraversal(matrix, x, y, teleporterMap) {
 function isInBounds(x, y, matrix) {
   return x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length 
 }
+
+
+
+
+//binary search in shifted arr 2 with dups
+
+// class Solution {
+//     public boolean search(int[] nums, int target) {
+//         int n = nums.length;
+//         if (n == 0) return false;
+//         int end = n - 1;
+//         int start = 0;
+
+//         while (start <= end) {
+//             int mid = start + (end - start) / 2;
+
+//             if (nums[mid] == target) {
+//                 return true;
+//             }
+
+//             if (!isBinarySearchHelpful(nums, start, nums[mid])) {
+//                 start++;
+//                 continue;
+//             }
+//             // which array does pivot belong to.
+//             boolean pivotArray = existsInFirst(nums, start, nums[mid]);
+
+//             // which array does target belong to.
+//             boolean targetArray = existsInFirst(nums, start, target);
+//             if (pivotArray ^ targetArray) { // If pivot and target exist in different sorted arrays, recall that xor is true when both operands are distinct
+//                 if (pivotArray) {
+//                     start = mid + 1; // pivot in the first, target in the second
+//                 } else {
+//                     end = mid - 1; // target in the first, pivot in the second
+//                 }
+//             } else { // If pivot and target exist in same sorted array
+//                 if (nums[mid] < target) {
+//                     start = mid + 1;
+//                 } else {
+//                     end = mid - 1;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+
+//     // returns true if we can reduce the search space in current binary search space
+//     private boolean isBinarySearchHelpful(int[] arr, int start, int element) {
+//         return arr[start] != element;
+//     }
+
+//     // returns true if element exists in first array, false if it exists in second
+//     private boolean existsInFirst(int[] arr, int start, int element) {
+//         return arr[start] <= element;
+//     }
+// }
+
+
+
+
+///binarySearch in shifted arr
+
+var search = function(nums, target) {
+    const pivot = findPivot(nums);
+
+    if (pivot === -1) {
+    return binarySearch(nums, target) 
+    }
+
+    if (target >= nums[0]) {
+    return binarySearch(nums.slice(0, pivot+1), target);
+    }
+
+    let res = binarySearch(nums.slice(pivot+1), target);
+
+    return res === -1 ? false : true;
+}
+
+
+function findPivot(arr) {
+    let l = 0;
+    let r = arr.length - 1;
+
+    while(l<=r) {
+        let mid = Math.floor((l+r)/2);
+
+        if(arr[mid] > arr[mid+1]) {
+          return mid
+        } else if (arr[mid] < arr[mid-1]) {
+          return mid-1;
+        } else if (arr[mid] > arr[0]) {
+          l = mid+1
+        } else {
+          r = mid - 1
+        }
+    }
+  
+   return -1;
+}
+
+function binarySearch(arr, target) {
+  if (!arr.length) {
+    return false
+  }
+  
+  const mid = Math.floor(arr.length/2);
+  if (arr[mid] === target) {
+    return true;
+  } else if (target < arr[mid]) {
+    return binarySearch(arr.slice(0, mid), target)
+  } else {
+    let res = binarySearch(arr.slice(mid+1), target)
+    return res === false ? false : true;
+  } 
+}
+
+
